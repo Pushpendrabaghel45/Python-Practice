596c7c56633581f3556372e07070a38c247d4333
Pushpendra baghel <bpushpendra501@gmail.com>
2025-08-04 12:42:07 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,76 @@
+# Ahamdabad = 10
+# Condition = '10'
+
+# # print(Ahamdabad == Condition)
+# # print(Ahamdabad > Condition)
+# # print(Ahamdabad > Condition)
+
+# print(Ahamdabad != Condition)
+
+
+# var = 10
+# # new_var = var +10
+# print(var) #10
+
+# var = var + 10
+# print(var) #20
+
+# var+=10 #var =var+10
+# print(var) #30
+
+
+# # print(var+1)
+# # print(var+1)
+# # print(var+1)
+
+# var+=1
+# print(var)   #31
+# var+=1
+# print(var)   #32
+# var+=1
+# print(var)   #33
+
+
+# # Calculate total price including discount
+# price = 200
+# discount = 15  # in percent
+# final_price = price - (price * discount / 100)
+# print("Final Price:", final_price)
+
+
+# age = int(input("Enter age: "))
+# if age >= 18:
+#     print("Eligible to vote")
+# else:
+#     print("Not eligible")
+
+
+
+# email = "admin@company.com"
+# password = "admin123"
+
+# if email == "admin@company.com" and password == "admin123":
+#     print("Login Success")
+# else:
+#     print("Invalid Credentials")
+
+
+# # User permissions using bits (1 = allowed, 0 = not)
+# READ = 4   # 100
+# WRITE = 2  # 010
+# EXEC = 1   # 001
+# print(READ )
+# print(WRITE)
+# print(EXEC)
+# user_perm = READ | EXEC  # 101
+# print(user_perm)  # Output: 5 (binary 101)
+# print(user_perm & WRITE == WRITE)  # False → No write permission
+
+
+
+# x = 10 + 5 * 2  # Output: 20 (Multiplication happens first)
+# y = (10 + 5) * 2  # Output: 30
+# print("x",x)  # 20
+# print("y",y)  # 30
+
+
