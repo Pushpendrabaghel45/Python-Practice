585f0ab347b100100380b99cca299d691b8f4d61
Pushpendra baghel <bpushpendra501@gmail.com>
2025-08-08 12:58:59 +0530

    Add files via upload

---
+++ DynamicInputs.py
@@ -0,0 +1,22 @@
+# vl1 = "50"
+# vl2 = "30"
+# vl2 = "30"
+# vl1= int(input("Hello frameboxx"))
+# vl2 = input("vl2 : " ) 
+
+# vl1= int(input("Hello frameboxx"))
+# vl2 = float(input("vl2 : " ) )
+
+# print(vl1*vl2)  
+# print (50+30)
+
+
+
+
+# print(100 / 10 * 10)
+# print(5 - 2 + 3)
+# print(5 - (2 + 3))
+# print(5 ** 3 ** 2)
+
+
+
