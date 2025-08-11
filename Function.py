e3966de2a0770b50cabd68a161bd5cdc0dea5484
Pushpendra baghel <bpushpendra501@gmail.com>
2025-08-11 14:04:56 +0530

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,49 @@
+
+# def fun():
+#     print("Hello, World!")
+# fun()
+
+
+# def evenOdd(x: int) -> str:
+#     if x % 2 == 0:
+#         return "Even"
+#     else:
+#         return "Odd"
+
+# print(evenOdd(16))
+# print(evenOdd(7))
+
+
+
+# def customfun():
+#     print("welcome")
+
+# print("Hello")
+
+# customfun()
+# print("test")
+# customfun()
+# print("sumit")
+# customfun()
+# print("Frameboxx")
+# customfun()
+
+
+
+def addition(x,y):
+    print(x+y)
+
+addition(10, 20)
+addition(30, 40)
+addition(50, 60)
+addition(70, 80)
+
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Alice")
+def greet(name):
+    return(f"Hello, {name}!")
+
+    print(greet('Alice'))
