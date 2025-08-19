c9aeffbbbd3b83c013b89a06a9dd5d03e85b14f1
Pushpendra baghel <bpushpendra501@gmail.com>
2025-08-19 13:48:31 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,79 @@
+# for(ini; condi;increment/decrement) {body}
+# for i in range(1, 10):
+#     for j in range(i):
+#         print(i, end=' ')
+#     print()
+
+
+# for i in range(10, 0, -1):
+#     for j in range(i):
+#         print("*", end=' ')
+#     print()
+
+
+
+# for i in range(1, 10):
+#     for j in range(10, i, -1):
+        
+#         print("*", end=' ')
+#     print()
+
+
+
+
+# li = ["python", "java", "c++", "javascript"]
+# for i in li:
+#     print(i)
+
+
+# tup = ("python", "java", "c++", "javascript")
+# for i in tup:
+#     print(i)
+
+
+
+# s = ("python")
+# for i in s:
+#     print(i)
+
+
+# li = ["python", "java", "c++", "javascript"]
+# for index in range(len(li)):
+#     print(li[index])
+# else:
+#     print("inside else block")
+
+
+
+# cnt = 0
+# while (cnt < 5):
+#     cnt = cnt + 1
+#     print("Hello World")
+
+# else:
+#     print("Inside else block")
+
+
+
+# # loop control statements
+# for p in 'pythonprogramming':
+#     if p == 'g' or p == 'r':
+#         continue  # Skip the rest of the loop for this iteration
+#     print(p)
+    
+
+# for p in 'pythonprogramming':
+#     if p in ('o'):
+#         break  # Exit the loop when 'g' or 'r' is encountered
+#     print(p)
+
+
+
+# for p in 'pythonprogramming':
+#     if p in ('g', 'r'):
+#         pass  # Do nothing for 'g' or 'r'
+#     print(p)  # Print other characters
+
+
+
+
