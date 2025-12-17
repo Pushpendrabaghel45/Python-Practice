028991faa0cc04d186618119ee687390ec979e07
Pushpendra baghel <bpushpendra501@gmail.com>
2025-12-17 13:11:13 +0530

    Update variable values and input prompts

--- DynamicInputs.py
+++ DynamicInputs.py
@@ -1,15 +1,15 @@
-# vl1 = "60"
-# vl2 = "30"
-# vl2 = "30"
-# vl1= int(input("Hello frameboxx"))
-# vl2 = input("vl2 : " ) 
+vl1 = "80"
+vl2 = "30"
+vl2 = "30"
+vl1= int(input("Hello frameboxx"))
+vl2 = input("vl2 : " ) 
 
-# vl1= int(input("Hello frameboxx"))
-# vl2 = float(input("vl2 : " ) )
+vl1= int(input("Hello frameboxx"))
+vl2 = float(input("vl2 : " ) )
 
-# print(vl1*vl2)  
-# print (60+30)
-#print (vl1/vl2)
+print(vl1*vl2)  
+print (80+30)
+print (vl1/vl2)
 
 
 
@@ -18,6 +18,7 @@
 # print(5 - 2 + 3)
 # print(5 - (2 + 3))
 # print(5 ** 3 ** 2)
+
 
 
 
