3873abe075b80ea444ee34cfe61f216ab5be0342
Pushpendra baghel <bpushpendra501@gmail.com>
2026-01-24 14:09:21 +0530

    Update ItraterGenrater.py

--- ItraterGenrater.py
+++ ItraterGenrater.py
@@ -1,8 +1,9 @@
 # 1. Create an iterator from a list of fruits and print each fruit using next().
 
-fruit = ["Apple", "Banana", "kavy"]
+fruit = ["Apple", "Banana", "kavy", "Orange"]
 it = iter(fruit)
 
+print(next(it))
 print(next(it))
 print(next(it))
 print(next(it))
