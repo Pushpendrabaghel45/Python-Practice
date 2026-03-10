e735bda177c67e7ba8e37b7a3f407d87711fcef7
Pushpendra baghel <bpushpendra501@gmail.com>
2026-03-10 11:17:27 +0530

    Uncommented tax, discount, and eligibility logic

--- Conditional.py
+++ Conditional.py
@@ -1,112 +1,113 @@
-# income = float(input("Enter your income: "))
+income = float(input("Enter your income: "))
 
-# if income <= 25000:
-#     print("No tax")
-# elif income <= 50000:
-#     print("Tax rate: 5%")
-# elif income <= 100000:
-#     print("Tax rate: 20%")
-# else:
-#     print("Tax rate: 30%")
-
-
-# amount = float(input("Enter purchase amount: "))
-# if amount >= 1000:
-#     print("You get a 10% discount")
-# else:
-#     print("No discount")
+if income <= 25000:
+    print("No tax")
+elif income <= 50000:
+    print("Tax rate: 5%")
+elif income <= 100000:
+    print("Tax rate: 20%")
+else:
+    print("Tax rate: 30%")
 
 
-# age = int(input("Enter your age: "))
-# if age >= 18:
-#     print("Eligible to vote")
-# else:
-#     print("Not eligible to vote")
+amount = float(input("Enter purchase amount: "))
+if amount >= 1000:
+    print("You get a 10% discount")
+else:
+    print("No discount")
 
 
-# student_score = int(input("Enter student score: "))
-# if student_score >= 90:
-#     print("Grade: A+")
-# elif student_score >= 80:
-#     print("Grade: A")
-# elif student_score >= 70:
-#     print("Grade: B")
-# elif student_score >= 60:
-#     print("Grade: C")
-# else:
-#     print("Grade: D")
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
 
 
-
-# result = int(input("Enter result score: "))
-# if result >= 33:     
-#     print("You passed the exam")
-# else:
-#     print("You failed the exam")    
-
-
-# first_name =str.upper (input("Enter your first name: "))
-# last_name =str.upper (input("Enter your last name: "))
-# Exam_year = int(input("Enter your exam year: "))
+student_score = int(input("Enter student score: "))
+if student_score >= 90:
+    print("Grade: A+")
+elif student_score >= 80:
+    print("Grade: A")
+elif student_score >= 70:
+    print("Grade: B")
+elif student_score >= 60:
+    print("Grade: C")
+else:
+    print("Grade: D")
 
 
 
-# English = float(input("Enter your marks English: "))
-# Math = float(input("Enter your marks Math: "))
-# Science = float(input("Enter your marks Science: "))
-# Commerce = float(input("Enter your marks Commerce: "))
-
-# total_marks =  English + Math + Science + Commerce
-# max_marks = 400
-# percentage = total_marks / 4
-
-# print(f"Student Name: {first_name} {last_name}")
-# print(f"Exam Year: {Exam_year}")
+result = int(input("Enter result score: "))
+if result >= 33:     
+    print("You passed the exam")
+else:
+    print("You failed the exam")    
 
 
-# if English < 50 or Math < 50 or Science < 50 or Commerce < 50:
-#     print("Result: Try Again")
-# else:
-#     print("Result: Pass")
+first_name =str.upper (input("Enter your first name: "))
+last_name =str.upper (input("Enter your last name: "))
+Exam_year = int(input("Enter your exam year: "))
 
-#     print("Marks Obtained:")  
-#     print(f"English: {English}")
-#     print(f"Math: {Math}")
-#     print(f"Science: {Science}")
-#     print(f"Commerce: {Commerce}")
 
-#     print(f"Total Marks: {total_marks}/{max_marks}")
 
-#     if percentage >= 90:
-#         grade = 'A+'
-#     elif percentage >= 80:
-#         grade = 'A'
-#     elif percentage >= 70:
-#         grade = 'B'
-#     elif percentage >= 60:
-#         grade = 'C'
-#     elif percentage >= 50:
-#         grade = 'D'
-#     else:
-#         grade = 'E'
+English = float(input("Enter your marks English: "))
+Math = float(input("Enter your marks Math: "))
+Science = float(input("Enter your marks Science: "))
+Commerce = float(input("Enter your marks Commerce: "))
+
+total_marks =  English + Math + Science + Commerce
+max_marks = 400
+percentage = total_marks / 4
+
+print(f"Student Name: {first_name} {last_name}")
+print(f"Exam Year: {Exam_year}")
+
+
+if English < 50 or Math < 50 or Science < 50 or Commerce < 50:
+    print("Result: Try Again")
+else:
+    print("Result: Pass")
+
+    print("Marks Obtained:")  
+    print(f"English: {English}")
+    print(f"Math: {Math}")
+    print(f"Science: {Science}")
+    print(f"Commerce: {Commerce}")
+
+    print(f"Total Marks: {total_marks}/{max_marks}")
+
+    if percentage >= 90:
+        grade = 'A+'
+    elif percentage >= 80:
+        grade = 'A'
+    elif percentage >= 70:
+        grade = 'B'
+    elif percentage >= 60:
+        grade = 'C'
+    elif percentage >= 50:
+        grade = 'D'
+    else:
+        grade = 'E'
     
-#     print(f"Percentage: {percentage:.2f}%")
-#     print(f"Grade: {grade}")
+    print(f"Percentage: {percentage:.2f}%")
+    print(f"Grade: {grade}")
 
 
 
-# department = input("Enter department: ")
-# role = input("Enter role: ")
+department = input("Enter department: ")
+role = input("Enter role: ")
 
-# if department == "IT":
-#     if role == "Developer":
-#         print("Assigned to Web App Project")
-#     elif role == "Tester":
-#         print("Assigned to QA Team")
-#     else:
-#         print("Unknown Role")
-# else:
-#     print("Not an IT department")
+if department == "IT":
+    if role == "Developer":
+        print("Assigned to Web App Project")
+    elif role == "Tester":
+        print("Assigned to QA Team")
+    else:
+        print("Unknown Role")
+else:
+    print("Not an IT department")
+
 
 
 
