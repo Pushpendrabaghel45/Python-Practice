dc3b4a769ee7840437988dd9170a192edc10db1c
Pushpendra baghel <bpushpendra501@gmail.com>
2026-03-12 14:42:38 +0530

    Uncomment and implement encapsulation examples

--- Encapsulation.py
+++ Encapsulation.py
@@ -12,44 +12,44 @@
 # 1. Create a class Account with a private attribute __balance. Add getter and setter methods.
 
 
+class Account:
+    def __init__(self, __balance):
+        self.__balance = __balance
 
-# class Account:
-#     def __init__(self, __balance):
-#         self.__balance = __balance
+    def get__balance(self):
+        return self.__balance
 
-#     def get__balance(self):
-#         return self.__balance
+    def set_balance(self,amount):
+        if amount >= 0:     
+            self.__balance = amount
 
-#     def set_balance(self,amount):
-#         if amount >= 0:     
-#             self.__balance = amount
-
-#         else:
-#             print("invalid amount")
-
+        else:
+            print("invalid amount")
 
 
-# acc = Account(100000)
-# print(acc.get__balance())
-# acc.set_balance(2000)
-# print(acc.get__balance())
+
+acc = Account(100000)
+print(acc.get__balance())
+acc.set_balance(2000)
+print(acc.get__balance())
 
 # 2.Make a class Car with public (brand), protected (_mileage), and private (__price) attributes. Print them.
 
 
-# class car:
-#     def __init__(self, brand, mileage, price):
-#         self.brand = brand
-#         self._mileage = mileage
-#         self.__price = price
+class car:
+    def __init__(self, brand, mileage, price):
+        self.brand = brand
+        self._mileage = mileage
+        self.__price = price
 
-#     def display(self):
-#         print(self.brand)
-#         print(self._mileage)
-#         print(self.__price)
+    def display(self):
+        print(self.brand)
+        print(self._mileage)
+        print(self.__price)
 
-# car = car("Thar", 18, 2000000)
-# car.display()
+car = car("Thar", 18, 2000000)
+car.display()
+
 
 
 
