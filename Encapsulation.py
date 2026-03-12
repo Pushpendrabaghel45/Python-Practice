561954f3711508130d823e4b4c7dbe9716631117
Pushpendra baghel <bpushpendra501@gmail.com>
2026-03-12 14:53:00 +0530

    Refactor car class and update its attributes

--- Encapsulation.py
+++ Encapsulation.py
@@ -12,29 +12,28 @@
 # 1. Create a class Account with a private attribute __balance. Add getter and setter methods.
 
 
-class Account:
-    def __init__(self, __balance):
-        self.__balance = __balance
+# class Account:
+#     def __init__(self, __balance):
+#         self.__balance = __balance
 
-    def get__balance(self):
-        return self.__balance
+#     def get__balance(self):
+#         return self.__balance
 
-    def set_balance(self,amount):
-        if amount >= 0:     
-            self.__balance = amount
+#     def set_balance(self,amount):
+#         if amount >= 0:     
+#             self.__balance = amount
 
-        else:
-            print("invalid amount")
+#         else:
+#             print("invalid amount")
 
 
 
-acc = Account(100000)
-print(acc.get__balance())
-acc.set_balance(2000)
-print(acc.get__balance())
+# acc = Account(100000)
+# print(acc.get__balance())
+# acc.set_balance(2000)
+# print(acc.get__balance())
 
 # 2.Make a class Car with public (brand), protected (_mileage), and private (__price) attributes. Print them.
-
 
 class car:
     def __init__(self, brand, mileage, price):
@@ -47,8 +46,9 @@ class car:
         print(self._mileage)
         print(self.__price)
 
-car = car("Thar", 18, 2000000)
+car = car("Thar", 20, 25000000)
 car.display()
+
 
 
 
