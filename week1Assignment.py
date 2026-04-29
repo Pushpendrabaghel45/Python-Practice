2770fee74252a35a06ced71ae68b79a93c7cfb7a
Pushpendra baghel <bpushpendra501@gmail.com>
2026-04-29 11:52:34 +0530

    Update week1Assignment.py

--- week1Assignment.py
+++ week1Assignment.py
@@ -23,7 +23,6 @@
 # 3. Write a program to calculate the area of a rectangle using user input for length and width.
 
 # length = float(input("Enter the length of rectangle:"))
-
 # width = float(input("Enter the width of rectangle:"))
 
 # area = length * width
