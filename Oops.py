f5e126aed1a3524c02b6e93db3a840d5e990dfbf
Pushpendra baghel <bpushpendra501@gmail.com>
2026-06-22 13:18:23 +0530

    Uncomment Car class and its methods in Oops.py

--- Oops.py
+++ Oops.py
@@ -50,29 +50,29 @@
                #  Step=3(Create 2 different car objects and call the method)
 
 
-# class Car:
-#     def __init__(self, brand, model, year):
-#         self.brand = brand
-#         self.model = model
-#         self.year = year
+class Car:
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year
 
-#     def start(self):
-#         print(f" {self.brand} {self.model} is starting.....")
+    def start(self):
+        print(f" {self.brand} {self.model} is starting.....")
 
-#     def display_info(self):
-#         print(f"Car : {self.year} {self.brand} {self.model}")
+    def display_info(self):
+        print(f"Car : {self.year} {self.brand} {self.model}")
 
-# # Create two car objects
-# car1 = Car("Honda", "Civic", 2021)
-# car2 = Car("Tata", "Nexon", 2022)
+# Create two car objects
+car1 = Car("Honda", "Civic", 2021)
+car2 = Car("Tata", "Nexon", 2022)
 
 
-# #call methods
-# car1.start()
-# car1.display_info()
+#call methods
+car1.start()
+car1.display_info()
 
-# car2.start()
-# car2.display_info()
+car2.start()
+car2.display_info()
 
 
                   #  Step=4( Add another method car_info() that returns brand, model, and year)
