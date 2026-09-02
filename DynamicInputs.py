1f35e5b579beb467d2f168ca1f6a187cd926abf9
Pushpendra baghel <bpushpendra501@gmail.com>
2026-09-02 15:31:53 +0530

    Update vl2 value and adjust print statements

--- DynamicInputs.py
+++ DynamicInputs.py
@@ -1,6 +1,6 @@
 vl1 = "80"
-vl2 = "30"
-vl2 = "30"
+vl2 = "40"
+vl2 = "40"
 vl1= int(input("Hello frameboxx"))
 vl2 = input("vl2 : " ) 
 
@@ -8,7 +8,7 @@ vl1= int(input("Hello frameboxx"))
 vl2 = float(input("vl2 : " ) )
 
 print(vl1*vl2)  
-print (80+30)
+print (80+40)
 print (vl1/vl2)
 
 # print(100 / 10 * 10)
