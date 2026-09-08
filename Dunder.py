a882fc86f9f2d3e48c2d7ff434f4827bbe3922bf
Pushpendra baghel <bpushpendra501@gmail.com>
2026-09-08 13:49:13 +0530

    Update Dunder.py

--- Dunder.py
+++ Dunder.py
@@ -1,11 +1,11 @@
 # # 1. Create a class Book with __str__ to display book details.
-
+     
 
 # class Book:
 #     def __init__(self,author, year):
 #         self.author = author
 #         self.year = year
-
+               
 #     def __str__(self):
 #         return f"Book details: , {self.author}, {self.year}"
 
